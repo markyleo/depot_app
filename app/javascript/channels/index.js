494fe1f438661a15aaa6fc10a68f34b2c5e369e6
ADSI <ADSI@admins-MacBook-Air.local>
2021-12-17 13:52:03 +0800

    Payment and Mailer

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,12 +1,14 @@
 // Load all the channels within this directory and all subdirectories.
 // Channel files must be named *_channel.js.
 
-const channels = require.context('.', true, /_channel\.js$/)
-channels.keys().forEach(channels)
+const channels = require.context('.', true, /_channel\.js$/);
+channels.keys().forEach(channels);
 
-cart = document.getElementById("cart")
-cart.innerHTML = "<%= j render(@cart) %>"
+cart = document.getElementById("cart");
+cart.innerHTML = "<%= j render(@cart) %>";
 
-notice = document.getElementById("notice")
-if notice
-notice.style.display = "none"
+notice = document.getElementById("notice");
+
+if(notice){
+    notice.style.display = "none";
+} 
