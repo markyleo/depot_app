99f4d7eb4404b2422f6980f333276ccaff4dccfe
ADSI <ADSI@admins-MacBook-Air.local>
2021-12-21 15:44:34 +0800

    iteration K2 pp 279

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,18 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
 
-class ActiveSupport::TestCase
+class ActionDispatch::IntegrationTest
+  def login_as(user)
+    post login_url, params: { name: user.name, password: 'secret' }
+  end
+  def logout
+    delete logout_url
+  end
+  def setup
+    login_as users(:one)
+  end
+end
+=begin class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
@@ -11,3 +22,6 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+ =end
+
+ 
